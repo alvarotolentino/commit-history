58320496d974527c503a0b17a7fa28f19d233dba
Alvaro Tolentino <alvarotolentinou@gmail.com>
2021-04-09 23:13:41 -0500

    Added Redis support for caching commit details

--- middlewares/redis-middleware.js
+++ middlewares/redis-middleware.js
@@ -7,5 +7,22 @@ const client = redis.createClient({
 });
 
 function redisMiddleware(req, res, next) {
-  
+  const key = req.url;
+
+  if (key) {
+    client.get(key, (err, reply) => {
+      if (err) {
+        res.status(500).json({ err: 'Something went wrong' });
+      }
+      if (reply !== null) {
+        res.json(JSON.parse(reply));
+      } else {
+        next();
+      }
+    });
+  } else {
+    next();
+  }
 }
+
+module.exports = redisMiddleware;
--- routes/github-routes.js
+++ routes/github-routes.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const redis = require('redis');
 const { Octokit } = require('@octokit/core');
+const redisMiddleware = require('../middlewares/redis-middleware');
 const ObjCommit = require('../models/Commits');
 const { OWNER, REPO, REDIS_HOST, REDIS_PORT } = require('../config');
 
 const octokit = new Octokit();
 
 const routes = express.Router();
+routes.use(redisMiddleware);
+let client = redis.createClient({
+  host: REDIS_HOST,
+  port: REDIS_PORT,
+});
 
 routes.get('/commits', async (req, res) => {
   const limit = req.query.limit || 10;
@@ -30,7 +37,9 @@ routes.get('/commits', async (req, res) => {
           res.json(commits.getSummary());
         }
       },
-      (err) => res.status(500).json({ err: 'Something went wrong' })
+      (err) => {
+        res.status(500).json({ err: 'Something went wrong' });
+      }
     );
 });
 
@@ -39,13 +48,15 @@ routes.get('/commits/:id', (req, res) => {
   if (id.length !== 40) res.json({ err: 'Invalid id' });
   else
     octokit
-      .request('GET /repos/{owner}/{repo}/commits', {
+      .request('GET /repos/{owner}/{repo}/commits/{id}', {
         owner: OWNER,
         repo: REPO,
+        id,
       })
       .then(
         (gitResponse) => {
           const detail = new ObjCommit(gitResponse.data);
+          client.set(req.url, JSON.stringify(detail.getDetails()));
           res.json(detail.getDetails());
         },
         (err) => res.status(500).json({ err: 'Something went wrong' })
