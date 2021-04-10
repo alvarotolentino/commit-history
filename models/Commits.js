7481cf18b20b25872085312ef436aea29675a956
Alvaro Tolentino <alvarotolentinou@gmail.com>
2021-04-09 22:39:43 -0500

    Fix getDetails function

--- models/Commits.js
+++ models/Commits.js
@@ -1,35 +1,34 @@
-const moment = require('moment');
-
 function Commits(data) {
   this.data = data;
-}
-
-Commits.prototype.getSummary = function getSummary() {
-  return this.data.map((item) => {
-    return {
-      sha: item.sha,
-      author: {
-        name: item.commit.author.name,
-        email: item.commit.author.email,
-        date: moment(item.commit.author.date).fromNow(),
-        avatar_url: item.author.avatar_url,
-      },
-      message: item.commit.message,
-    };
-  });
-};
-
-Commits.prototype.getDetails = function getDetails() {
-  return {
-    sha: this.data[0].sha,
-    author: {
-      name: this.data[0].commit.author.name,
-      email: this.data[0].commit.author.email,
-      date: moment(this.data[0].commit.author.date).fromNow(),
-    },
-    message: this.data[0].commit.message,
-    html_url: this.data[0].html_url
+  this.getSummary = () => {
+    return this.data.map((item) => {
+      return {
+        sha: item.sha,
+        author: {
+          name: item.commit.author.name,
+          email: item.commit.author.email,
+          date: item.commit.author.date,
+          avatar_url: item.author.avatar_url,
+        },
+        message: item.commit.message,
+      };
+    });
   };
-};
+
+  this.getDetails = () => {
+    return {
+      sha: this.data.sha,
+      author: {
+        name: this.data.commit.author.name,
+        email: this.data.commit.author.email,
+        date: this.data.commit.author.date,
+        avatar_url: this.data.author.avatar_url,
+
+      },
+      message: this.data.commit.message,
+      html_url: this.data.html_url,
+    };
+  };
+}
 
 module.exports = Commits;
