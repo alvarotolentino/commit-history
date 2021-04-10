8877d79777e53eac367369e39fd7d208135b1110
Alvaro Tolentino <alvarotolentinou@gmail.com>
2021-04-09 23:13:19 -0500

    Added CORS support

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const githubRoutes = require('./routes/github-routes');
 const { BACKEND_PORT } = require('./config');
 
 const app = express();
-
+app.use((req,res,next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  res.header('Access-Control-Allow-Credentials', true);
+  next();
+})
 app.use('/', githubRoutes);
 
 app.listen(BACKEND_PORT, () => {
